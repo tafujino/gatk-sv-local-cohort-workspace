257ec4816d9ef31fdb0244c12e3331f79f914451
Takeshi Fujino <fujino@edu.k.u-tokyo.ac.jp>
2025-04-22 15:51:28 +0900

    Fix gatk-sv version

--- script/lib/settings.rb
+++ script/lib/settings.rb
@@ -2,7 +2,7 @@
 
 require 'pathname'
 
-GATK_SV_VERSION = '2871327daa17762a62a8cb3ad1eefd50daac6761'
+GATK_SV_VERSION = 'v1.0.3'
 CROMWELL_DIR = Pathname.new('cromwell')
 CROMWELL_NETWORK_CONF_PATH = CROMWELL_DIR / 'network.json'
 WORKSPACE_DIR = Pathname.new('workspace')
